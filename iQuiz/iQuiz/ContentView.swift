75d6871d2c9a93bb61f0028c771501802ede38f5
israelrmartinez <israel.r.mar@gmail.com>
2022-05-17 18:16:08 -0700

    json data is still accessed

--- iQuiz/iQuiz/ContentView.swift
+++ iQuiz/iQuiz/ContentView.swift
@@ -22,37 +22,39 @@ struct ContentView: View {
     @State var isActive: Bool = false
     @State private var results = [Result]()
     @State var posts: [Post] = []
+    @State var quizzes: [Quiz] = []
+    @State var arr: [Quiz] = []
     
-    @State var quizzes: [Quiz] = [
-        Quiz(quiz: [ "title":"Marvel Super Heroes", "desc": "Avengers, Assemble!",
-               "questions":[
-                 ["text":"Who is Iron Man?",
-                   "answer":"1",
-                   "answers":[
-                     "Tony Stark",
-                     "Obadiah Stane",
-                     "A rock hit by Megadeth",
-                     "Nobody knows"]
-                 ],
-                 ["text":"Who founded the X-Men?",
-                   "answer":"2",
-                   "answers":[
-                     "Tony Stark",
-                     "Professor X",
-                     "The X-Institute",
-                     "Erik Lensherr"]
-                 ],
-                 ["text":"How did Spider-Man get his powers?",
-                   "answer":"1",
-                   "answers":[
-                     "He was bitten by a radioactive spider",
-                     "He ate a radioactive spider",
-                     "He is a radioactive spider",
-                     "He looked at a radioactive spider"]
-                 ]
-               ]
-         ])
-    ]
+//    @State var quizzes: [Quiz] = [
+//        Quiz(quiz: [ "title":"Marvel Super Heroes", "desc": "Avengers, Assemble!",
+//               "questions":[
+//                 ["text":"Who is Iron Man?",
+//                   "answer":"1",
+//                   "answers":[
+//                     "Tony Stark",
+//                     "Obadiah Stane",
+//                     "A rock hit by Megadeth",
+//                     "Nobody knows"]
+//                 ],
+//                 ["text":"Who founded the X-Men?",
+//                   "answer":"2",
+//                   "answers":[
+//                     "Tony Stark",
+//                     "Professor X",
+//                     "The X-Institute",
+//                     "Erik Lensherr"]
+//                 ],
+//                 ["text":"How did Spider-Man get his powers?",
+//                   "answer":"1",
+//                   "answers":[
+//                     "He was bitten by a radioactive spider",
+//                     "He ate a radioactive spider",
+//                     "He is a radioactive spider",
+//                     "He looked at a radioactive spider"]
+//                 ]
+//               ]
+//         ])
+//    ]
     
 //    var xQuizzes = [
 //        XQuiz()
@@ -101,17 +103,24 @@ struct ContentView: View {
             
             let httpResponse = response! as! HTTPURLResponse
             
-            print(data ?? [{}])
+            print("data: ", data ?? [[]], "the data")
             
             do {
-                let questions = try JSONSerialization.jsonObject(with: data!)
-                print(questions)
+                let getQuizzes = try JSONSerialization.jsonObject(with: data!)
+                print("questions: \(getQuizzes)")
+                print("type: \(type(of: getQuizzes))")
+//                itemsDownloaded(items: getQuizzes as! [AnyObject])
             }
             catch {
                 print("Something went boom")
             }
         }
         session.resume()
+    }
+    
+    func itemsDownloaded(items: [AnyObject]!) {
+        var quzzes = items as NSArray as! [Quiz]
+        quzzes.forEach {_ in print(quzzes[0])}
     }
 }
 
